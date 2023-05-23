4a374773d801522e3d9a9d36b50210c34499ae3e
adatz273 <ali.datz@web.de>
2023-05-23 18:19:02 +0200

    include small script to setup environment

--- geant4init.sh
+++ geant4init.sh
@@ -2,7 +2,7 @@
 
 #setup environment for geant4
 export G4DIR=~/Software/Geant4/geant4-v11.1.1-install/lib/Geant4-11.1.1
-source /home/alexandra/Software/Geant4/geant4-v11.1.1-install/bin/geant4.sh
+source ~/Software/Geant4/geant4-v11.1.1-install/bin/geant4.sh
 
 
 
